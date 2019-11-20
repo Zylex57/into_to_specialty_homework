1588eb096b076ac32be0f356166704723ad970ce
Vitalii Kruk <zylex57@gmail.com>
2019-11-20 20:58:05 +0200

    adding a main file with input and output

---
+++ Source.cpp
@@ -0,0 +1,19 @@
+#include "Komaxa.h"
+int main()
+{
+	Komaxa A;
+	A.Input();
+	A.Print();
+
+
+	Komaxa B;
+	B.Input();
+	B.Print();
+
+	Komaxa C;
+	C.Input();
+	C.Print();
+
+	system("pause");
+	return 0;
+}
